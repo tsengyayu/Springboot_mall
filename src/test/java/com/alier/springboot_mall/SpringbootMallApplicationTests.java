ddab4ae21568268c91b5660d2344df405d2237ef
yayu <zengalier@gmail.com>
2022-09-13 00:26:17 +0800

    初始化springboot project

---
+++ src/test/java/com/alier/springboot_mall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alier.springboot_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
